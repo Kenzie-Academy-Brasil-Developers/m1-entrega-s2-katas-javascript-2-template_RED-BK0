114654735e52db90fb719cc4e431c625e717353f
RED BK <brunokelven0@gmail.com>
2022-10-25 08:29:52 -0400

    finalizaçao

--- katas2.js
+++ katas2.js
@@ -1,1 +1,29 @@
-
+function add(num1, num2){
+    return num1 + num2
+ }
+ 
+ function multipy (num1, num2){
+    let resultMultiply = 0
+    for (let i = 1; i <= num2; i++){
+ 
+        resultMultiply =  add(resultMultiply, num1) 
+    }
+    return resultMultiply
+ }
+ 
+ function power (x, n){
+    let resultPower = 1;
+    for (let index = 0; index < n; index++){      
+        resultPower =  multipy(resultPower, x) 
+    }
+    return resultPower
+ }
+ 
+ function factorial(number){
+    let result = number;
+    for (let index = 1; index < number; index++){
+        result = multipy(result, number - index)
+    }
+    return result 
+ }
+ console.log(multipy(10, 5))
